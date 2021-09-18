4470a3ee1e3536deaf94e39aa89b5bfe86ba1f02
lbistech <65247630+lbistech@users.noreply.github.com>
2021-09-18 22:39:47 +0500

    Created devops batch04 directory with bash scripts
    Here you have bash scripts

---
+++ Batch-04/Bash Scripts/file_creation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hello, bash scripting - linux course" > linux.txt
+
+echo "This is the second line of the txt file" >> linux.txt
---
+++ Batch-04/Bash Scripts/input.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+FirstName=$1
+SecondName=$2
+CourseName=$3
+BatchNumber=$4
+
+echo "-------------------------"
+echo "My First Name is $FirstName"
+echo "-------------------------"
+echo "My Second Name is $SecondName"
+echo "-------------------------"
+echo "My Training Course Name is $CourseName"
+echo "-----------------------------"
+echo "My Batch Number is $BatchNumber"
+echo "-----------------------------"
+echo "This is my fifth arg $5"
+
