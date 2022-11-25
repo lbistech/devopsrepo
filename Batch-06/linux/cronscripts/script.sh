94bf8a05cd1bc3f1c2d8f9a98d2593590546360b
Usman <usman@lbistech.com>
2022-11-25 13:22:22 +0500

    added batch06 linux content

---
+++ Batch-06/linux/cronscripts/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "--- These are the system level commands ---"
+
+#Tell the user that the script is starting
+echo "---Starting the system information script ---"
+
+# Display the hostname of the system
+echo "--- My System hostname is ---"
+hostname
+
+# Display the current date and time of the system
+echo "--- The current date and time is ---"
+date
+
+# Display the kernel release followed by the architecture
+echo "--- Kernel level information ---"
+uname -r
+uname -m
+
+# Display the dish usages
+df -h
+
+# End the script
+echo "Stopping the system information script"
