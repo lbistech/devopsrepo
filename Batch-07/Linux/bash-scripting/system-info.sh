a55383f729a7782f176aae8e9b399aa5cc26e506
Ubuntu <ubuntu@ip-172-31-39-167.eu-west-1.compute.internal>
2023-11-26 10:39:07 +0000

    created batch07 dir for linux

---
+++ Batch-07/Linux/bash-scripting/system-info.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "----------------------------------------------------"
+echo "--- Here you have your system information script ---"
+echo "----------------------------------------------------"
+
+echo "--- My System Hostname is ---"
+hostname
+
+echo "--- The current date and time is ---"
+date
+
+echo "--- Display the disk usage details ---"
+df -h
+
+echo "-------------------------------------"
+echo "--- Thanks for running the script ---"
+echo "-------------------------------------"
