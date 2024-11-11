5f9203def456a6014efeaf9de9a4bf7a01b517e0
Ubuntu <ubuntu@ip-172-31-29-119.us-east-2.compute.internal>
2024-11-11 13:31:10 +0000

    Linux data is added

---
+++ Batch-08/Linux-Data/bash_scripting/Call_recording_backup_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "------------------------------------"
+echo "--- Call recording backup script ---"
+echo "------------------------------------"
+
+SOURCE_DIR="/home/ubuntu/call_recordings"
+BACKUP_DIR="/home/ubuntu/call_backups"
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+BACKUP_PATH="$BACKUP_DIR/backup_$TIMESTAMP"
+
+if [ ! -d "$BACKUP_DIR" ]; then
+mkdir -p "$BACKUP_DIR"
+echo "Backup directory has been created at $BACKUP_DIR"
+fi
+
+mkdir $BACKUP_PATH
+
+echo "Backup directory has been created at $BACKUP_PATH"
+cp -r "$SOURCE_DIR"/* "$BACKUP_PATH"
+echo "Files from $SOURCE_DIR have been backup to $BACKUP_PATH"
+echo "Backup completed successfully at $(date)"
